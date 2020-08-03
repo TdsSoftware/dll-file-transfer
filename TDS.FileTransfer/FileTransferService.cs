54e17af6c603d5d348f14d55d5430476d354dd0b
matheus <matheus@tdssistemas.onmicrosoft.com>
2020-08-03 15:37:46 -0300

    Add project files.

---
+++ TDS.FileTransfer/FileTransferService.cs
@@ -0,0 +1,35 @@
+﻿using Renci.SshNet;
+using System;
+using System.IO;
+
+namespace TDS.FileTransfer
+{
+    public static class FileTransferService
+    {
+        [DllExport]
+        public static string UploadSFTP(string host, int port, string username, string password, string remoteDirectory, string uploadfile)
+        {
+            using (SftpClient sftp = new SftpClient(host, port, username, password))
+            {
+                try
+                {
+                    sftp.Connect();
+
+                    using (var fileStream = new FileStream(uploadfile, FileMode.Open))
+                    {
+                        var nomeArquivo = Path.GetFileName(fileStream.Name);
+                        sftp.UploadFile(fileStream, remoteDirectory + "/" + nomeArquivo);
+                    }
+
+                    sftp.Disconnect();
+                }
+                catch (Exception e)
+                {
+                    return e.ToString();
+                }
+            }
+
+            return "OK";
+        }
+    }
+}
